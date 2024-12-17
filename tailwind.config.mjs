aa7007c33e5b4ba26fa3e91e99b373f3e9020274
Line <lith0001@stud.kea.dk>
2024-12-17 11:33:17 +0100

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -157,7 +157,7 @@ export default {
                   from: { transform: 'translateX(0)' },
                   to: { transform: 'translateX(-100%)' },
                 },
-              }
+              },
         },
 	},
 	plugins: [
