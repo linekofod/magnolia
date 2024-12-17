3387ad2f58ce32b6c6b0125e6f55dcf9433a87b4
”Brandtned” <mara0008@stud.kea.dk>
2024-12-17 15:13:52 +0100

    Mb

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -159,11 +159,12 @@ export default {
                 },
               },
         },
-	},
+
+	   
 	plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
-}
+}}
